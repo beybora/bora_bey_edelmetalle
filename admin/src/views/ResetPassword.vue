9c94d70435999e4510a226b3ff797c3fa91cac35
beybora <bora.b.sarikaya@gmail.com>
2025-05-17 23:55:16 +0200

    refactor login, resetpassword and request new password pages with reusable AuthLayout component

--- admin/src/views/ResetPassword.vue
+++ admin/src/views/ResetPassword.vue
@@ -1,73 +1,59 @@
 <template>
-    <div
-        class="flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8"
-    >
-        <div class="sm:mx-auto sm:w-full sm:max-w-sm">
-            <img
-                class="mx-auto h-10 w-auto"
-                src="https://tailwindcss.com/plus-assets/img/logos/mark.svg?color=indigo&shade=600"
-                alt="Your Company"
-            />
-            <h2
-                class="mt-10 text-center text-2xl/9 font-bold tracking-tight text-gray-900"
-            >
-                Set new password
-            </h2>
-        </div>
-
-        <div class="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
-            <form class="space-y-6" action="#" method="POST">
-                <div>
-                    <div class="flex items-center justify-between">
-                        <label
-                            for="new-password"
-                            class="block text-sm/6 font-medium text-gray-900"
-                        >
-                            New Password
-                        </label>
-                    </div>
-                    <div class="mt-2">
-                        <input
-                            type="password"
-                            name="new-password"
-                            id="new-password"
-                            required
-                            class="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6"
-                            placeholder="Enter new password"
-                        />
-                    </div>
-                </div>
-
-                <div>
-                    <div class="flex items-center justify-between">
-                        <label
-                            for="confirm-password"
-                            class="block text-sm/6 font-medium text-gray-900"
-                        >
-                            Confirm New Password
-                        </label>
-                    </div>
-                    <div class="mt-2">
-                        <input
-                            type="password"
-                            name="confirm-password"
-                            id="confirm-password"
-                            required
-                            class="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6"
-                            placeholder="Confirm new password"
-                        />
-                    </div>
-                </div>
-
-                <div>
-                    <button
-                        type="submit"
-                        class="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm/6 font-semibold text-white shadow-xs hover:bg-indigo-500 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+    <AuthLayout title="Reset Password">
+        <form class="space-y-6" action="#" method="POST">
+            <div>
+                <div class="flex items-center justify-between">
+                    <label
+                        for="new-password"
+                        class="block text-sm/6 font-medium text-gray-900"
                     >
-                        Set New Password
-                    </button>
+                        New Password
+                    </label>
                 </div>
-            </form>
-        </div>
-    </div>
+                <div class="mt-2">
+                    <input
+                        type="password"
+                        name="new-password"
+                        id="new-password"
+                        required
+                        class="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6"
+                        placeholder="Enter new password"
+                    />
+                </div>
+            </div>
+
+            <div>
+                <div class="flex items-center justify-between">
+                    <label
+                        for="confirm-password"
+                        class="block text-sm/6 font-medium text-gray-900"
+                    >
+                        Confirm New Password
+                    </label>
+                </div>
+                <div class="mt-2">
+                    <input
+                        type="password"
+                        name="confirm-password"
+                        id="confirm-password"
+                        required
+                        class="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6"
+                        placeholder="Confirm new password"
+                    />
+                </div>
+            </div>
+
+            <div>
+                <button
+                    type="submit"
+                    class="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm/6 font-semibold text-white shadow-xs hover:bg-indigo-500 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+                >
+                    Set New Password
+                </button>
+            </div>
+        </form>
+    </AuthLayout>
 </template>
+<script setup>
+import AuthLayout from "../components/AuthLayout.vue";
+</script>
