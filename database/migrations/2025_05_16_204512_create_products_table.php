72386b83198e270f629750bdb5e4d5fc0afc9253
beybora <bora.b.sarikaya@gmail.com>
2025-05-17 11:20:30 +0200

    write initial database migrations

--- database/migrations/2025_05_16_204512_create_products_table.php
+++ database/migrations/2025_05_16_204512_create_products_table.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Models\User;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
@@ -13,6 +14,16 @@ return new class extends Migration
     {
         Schema::create('products', function (Blueprint $table) {
             $table->id();
+            $table->string('title', 2000);
+            $table->string('slug', 2000)->nullable();
+            $table->string('image')->nullable();
+            $table->string('image_mime')->nullable();
+            $table->integer('image_size')->nullable();
+            $table->longText('description')->nullable();
+            $table->foreignIdFor(User::class, 'crated_by')->nullable();
+            $table->foreignIdFor(User::class, 'updated_by')->nullable();
+            $table->foreignIdFor(User::class, 'deleted_by')->nullable();
+            $table->softDeletes();
             $table->timestamps();
         });
     }
