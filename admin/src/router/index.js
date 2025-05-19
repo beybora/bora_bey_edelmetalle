cf03f50261e35f626ea8a97653b17a99add4b38a
beybora <bora.b.sarikaya@gmail.com>
2025-05-19 17:04:03 +0200

    add authentication and implement global route guards

--- admin/src/router/index.js
+++ admin/src/router/index.js
@@ -7,12 +7,16 @@ import Login from "../views/Login.vue";
 import RequestPassword from "../views/RequestPassword.vue";
 import ResetPassword from "../views/ResetPassword.vue";
 import AppLayout from "../components/AppLayout.vue";
+import store from "../store/index.js";
 
 const routes = [
     {
         path: "/app",
         name: "app",
         component: AppLayout,
+        meta: {
+            requiresAuth: true,
+        },
         children: [
             {
                 path: "dashboard",
@@ -39,16 +43,25 @@ const routes = [
     {
         path: "/login",
         name: "login",
+        meta: {
+            requiresUnauth: true,
+        },
         component: Login,
     },
     {
         path: "/request-password",
         name: "requestPassword",
+        meta: {
+            requiresUnauth: true,
+        },
         component: RequestPassword,
     },
     {
         path: "/reset-password",
         name: "resetPassword",
+        meta: {
+            requiresUnauth: true,
+        },
         component: ResetPassword,
     },
 ];
@@ -56,6 +69,16 @@ const routes = [
 const router = createRouter({
     history: createWebHistory(),
     routes,
+});
+
+router.beforeEach((to, from, next) => {
+    if (to.meta.requiresAuth && !store.state.user.token) {
+        next({ name: "login" });
+    } else if (to.meta.requiresUnauth && store.state.user.token) {
+        next({ name: "app.dashboard" });
+    } else {
+        next();
+    }
 });
 
 export default router;
