4b2ef91e0e8a68f7abec2e5894d368b64e6e6f4d
beybora <bora.b.sarikaya@gmail.com>
2025-05-19 12:08:27 +0200

    add sidebar navigation

---
+++ admin/src/components/Sidebar.vue
@@ -0,0 +1,37 @@
+<template>
+    <div
+        class="w-[200px] bg-indigo-700 text-gray-300 py-4 px-2 space-y-2 rounded-r-sm shadow-lg"
+    >
+        <router-link
+            :to="{ name: 'app.dashboard' }"
+            class="flex items-center py-1 px-2 rounded hover:bg-black/30"
+        >
+            <span>Dashboard </span>
+        </router-link>
+        <router-link
+            :to="{ name: 'app.products' }"
+            class="flex items-center py-1 px-2 rounded hover:bg-black/30"
+        >
+            <span>Products </span>
+        </router-link>
+        <router-link
+            :to="{ name: 'app.users' }"
+            class="flex items-center py-1 px-2 rounded hover:bg-black/30"
+        >
+            <span>Users </span>
+        </router-link>
+        <router-link
+            :to="{ name: 'app.reports' }"
+            class="flex items-center py-1 px-2 rounded hover:bg-black/30"
+        >
+            <span>Reports </span>
+        </router-link>
+    </div>
+</template>
+<script setup>
+defineProps({
+    title: {
+        type: String,
+    },
+});
+</script>
