c35941f40e5f397382db92a08a11c245726c472e
beybora <bora.b.sarikaya@gmail.com>
2025-05-19 12:08:18 +0200

    setup nested routes for AppLayout and create pages for dashboard, products, users, and reports

---
+++ admin/src/components/AppLayout.vue
@@ -0,0 +1,22 @@
+<template>
+    <div class="min-h-full flex">
+        <Sidebar />
+
+        <div class="flex-1">
+            <header class="h-8 shadow bg-white">Header</header>
+
+            <main>
+                <router-view></router-view>
+            </main>
+        </div>
+    </div>
+</template>
+
+<script setup>
+import Sidebar from "./Sidebar.vue";
+defineProps({
+    title: {
+        type: String,
+    },
+});
+</script>
--- admin/src/router/index.js
+++ admin/src/router/index.js
@@ -1,14 +1,40 @@
 import { createRouter, createWebHistory } from "vue-router";
-import Dashbard from "../views/Dashboard.vue";
+import Dashboard from "../views/Dashboard.vue";
+import Users from "../views/Users.vue";
+import Reports from "../views/Reports.vue";
+import Products from "../views/Products.vue";
 import Login from "../views/Login.vue";
 import RequestPassword from "../views/RequestPassword.vue";
 import ResetPassword from "../views/ResetPassword.vue";
+import AppLayout from "../components/AppLayout.vue";
 
 const routes = [
     {
-        path: "/dashboard",
-        name: "dashboard",
-        component: Dashbard,
+        path: "/app",
+        name: "app",
+        component: AppLayout,
+        children: [
+            {
+                path: "dashboard",
+                name: "app.dashboard",
+                component: Dashboard,
+            },
+            {
+                path: "products",
+                name: "app.products",
+                component: Products,
+            },
+            {
+                path: "users",
+                name: "app.users",
+                component: Users,
+            },
+            {
+                path: "reports",
+                name: "app.reports",
+                component: Reports,
+            },
+        ],
     },
     {
         path: "/login",
