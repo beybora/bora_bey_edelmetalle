bdafc3c94e6b3487b6ff6cbeecbd1d3baa5a7a69
beybora <bora.b.sarikaya@gmail.com>
2025-05-22 21:58:13 +0200

    add error message handling for login view

--- admin/src/views/Login.vue
+++ admin/src/views/Login.vue
@@ -1,6 +1,13 @@
 <template>
     <AuthLayout title="Login to your account">
         <form class="space-y-6" @submit.prevent="login" method="POST">
+            <div
+                v-if="errorMsg"
+                class="flex items-center justify-center py-3 px-5 bg-red-400 text-white rounded-md"
+                role="alert"
+            >
+                {{ errorMsg }}
+            </div>
             <div>
                 <label
                     for="email"
@@ -51,9 +58,26 @@
 
             <div>
                 <button
+                    :disabled="loading"
+                    :class="{
+                        'cursor-not-allowed opacity-50': loading,
+                        'hover:bg-indigo-500': loading,
+                    }"
                     type="submit"
                     class="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm/6 font-semibold text-white shadow-xs hover:bg-indigo-500 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
                 >
+                    <svg
+                        v-if="loading"
+                        class="animate-spin -ml-1 mr-3 h-5 w-5 text-white"
+                        xmlns="http://www.w3.org/2000/svg"
+                        viewBox="0 0 24 24"
+                    >
+                        <path
+                            class="opacity-75"
+                            fill="currentColor"
+                            d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
+                        ></path>
+                    </svg>
                     Sign in
                 </button>
             </div>
