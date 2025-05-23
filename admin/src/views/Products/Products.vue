f4aa7d9d7114fe169f66b2738a281897f1323b49
beybora <bora.b.sarikaya@gmail.com>
2025-05-23 12:59:37 +0200

    add modal component for product creation

--- admin/src/views/Products/Products.vue
+++ admin/src/views/Products/Products.vue
@@ -3,16 +3,22 @@
         <div class="flex items-center justify-between mb-3">
             <h1 class="text-3xl font-semibold">Products</h1>
             <button
-                type="submit"
+                @click="showModal = true"
                 class="py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
             >
                 Add new product
             </button>
         </div>
+
         <ProductsTable />
+        <ProductModal v-model="showModal" />
     </div>
 </template>
 
 <script setup>
+import { ref } from "vue";
+import ProductModal from "./ProductModal.vue";
 import ProductsTable from "./ProductsTable.vue";
+
+const showModal = ref(false);
 </script>
