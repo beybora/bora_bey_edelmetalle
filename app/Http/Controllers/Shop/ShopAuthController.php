55ba4764350b4097d2404cfeaf55938c944ed1c5
beybora <bora.b.sarikaya@gmail.com>
2025-05-26 22:53:50 +0200

    add register functionality to ShopAuthController

--- app/Http/Controllers/Shop/ShopAuthController.php
+++ app/Http/Controllers/Shop/ShopAuthController.php
@@ -4,11 +4,34 @@ namespace App\Http\Controllers\Shop;
 
 use App\Http\Controllers\Controller;
 use App\Http\Resources\UserResource;
+use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
 
 class ShopAuthController extends Controller
 {
+    public function register(Request $request)
+    {
+        $data = $request->validate([
+            'name' => ['required', 'string', 'max:255'],
+            'email' => ['required', 'email', 'unique:users,email'],
+            'password' => ['required', 'confirmed'],
+        ]);
+
+        $data['password'] = Hash::make($data['password']);
+
+        $user = User::create($data);
+
+        $token = $user->createToken('shop')->plainTextToken;
+
+        return response([
+            'message' => 'Registrierung erfolgreich',
+            'token' => $token,
+            'user' => new UserResource($user),
+        ], 201);
+    }
+
     public function login(Request $request)
     {
         $credentials = $request->validate([
