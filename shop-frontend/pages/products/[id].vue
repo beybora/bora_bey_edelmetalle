254f68f56648a685b841da0372a40c4d27cd302c
beybora <bora.b.sarikaya@gmail.com>
2025-05-27 22:44:54 +0200

    add dynamic routing for product detail page, create ProductDetail.vue

---
+++ shop-frontend/pages/products/[id].vue
@@ -0,0 +1,10 @@
+<template>
+    <ShopLayout>
+        <ProductDetail />
+    </ShopLayout>
+</template>
+
+<script setup>
+import ShopLayout from "~/components/layouts/ShopLayout.vue";
+import ProductDetail from "~/components/ProductDetail.vue";
+</script>
