bcc6d0e6b524bf4a664ae4cf9d78f29416f9d562
beybora <bora.b.sarikaya@gmail.com>
2025-05-30 23:57:41 +0200

    backend: modify Order, OrderDetail, OrderItem models with relations

--- app/Models/Order.php
+++ app/Models/Order.php
@@ -6,5 +6,20 @@ use Illuminate\Database\Eloquent\Model;
 
 class Order extends Model
 {
-    //
+    protected $fillable = ['total_price', 'status', 'created_by'];
+
+    public function items()
+    {
+        return $this->hasMany(OrderItem::class);
+    }
+
+    public function details()
+    {
+        return $this->hasOne(OrderDetail::class);
+    }
+
+    public function user()
+    {
+        return $this->belongsTo(User::class, 'created_by');
+    }
 }
--- app/Models/OrderDetail.php
+++ app/Models/OrderDetail.php
@@ -6,5 +6,21 @@ use Illuminate\Database\Eloquent\Model;
 
 class OrderDetail extends Model
 {
-    //
+    protected $fillable = [
+        'order_id',
+        'first_name',
+        'last_name',
+        'phone',
+        'address1',
+        'address2',
+        'city',
+        'state',
+        'zipcode',
+        'country_code'
+    ];
+
+    public function order()
+    {
+        return $this->belongsTo(Order::class);
+    }
 }
