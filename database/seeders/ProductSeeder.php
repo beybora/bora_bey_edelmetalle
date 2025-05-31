70e7bab30054abbaea85a26df6ce519ba6f0cb15
beybora <bora.b.sarikaya@gmail.com>
2025-05-31 03:11:59 +0200

    backend: add working image links to product seeder

--- database/seeders/ProductSeeder.php
+++ database/seeders/ProductSeeder.php
@@ -12,11 +12,35 @@ class ProductSeeder extends Seeder
     {
         $categories = Category::all();
 
+        $imageUrls = [
+            "https://picsum.photos/id/10/400/300",
+            "https://picsum.photos/id/20/400/300",
+            "https://picsum.photos/id/30/400/300",
+            "https://picsum.photos/id/40/400/300",
+            "https://picsum.photos/id/50/400/300",
+            "https://picsum.photos/id/60/400/300",
+            "https://picsum.photos/id/70/400/300",
+            "https://picsum.photos/id/80/400/300",
+            "https://picsum.photos/id/90/400/300",
+            "https://picsum.photos/id/100/400/300",
+            "https://picsum.photos/id/110/400/300",
+            "https://picsum.photos/id/120/400/300",
+            "https://picsum.photos/id/130/400/300",
+            "https://picsum.photos/id/140/400/300",
+            "https://picsum.photos/id/150/400/300",
+            "https://picsum.photos/id/160/400/300",
+            "https://picsum.photos/id/170/400/300",
+            "https://picsum.photos/id/180/400/300",
+            "https://picsum.photos/id/190/400/300",
+            "https://picsum.photos/id/200/400/300",
+        ];
+
         Product::factory()
             ->count(20)
             ->make()
-            ->each(function ($product) use ($categories) {
+            ->each(function ($product, $index) use ($categories, $imageUrls) {
                 $product->category_id = $categories->random()->id ?? null;
+                $product->image = $imageUrls[$index];
                 $product->save();
             });
     }
