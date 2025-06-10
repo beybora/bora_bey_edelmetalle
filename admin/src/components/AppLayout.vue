f43a6b5b59ad106429c2044020b0f49a7eb273d5
beybora <bora.b.sarikaya@gmail.com>
2025-06-10 22:26:26 +0200

    admin: remove sidebar open/close logic from layout and sidebar components

--- admin/src/components/AppLayout.vue
+++ admin/src/components/AppLayout.vue
@@ -1,14 +1,15 @@
 <template>
     <div v-if="currentUser.id" class="min-h-full flex">
-        <Sidebar :is-open="isSidebarOpen" />
+        <Sidebar />
 
         <div class="flex-1">
-            <AdminHeader @toggle-sidebar="toggleSidebar" />
+            <AdminHeader />
             <main class="p-5">
                 <router-view></router-view>
             </main>
         </div>
     </div>
+
     <div
         v-else
         class="flex items-center justify-center min-h-full bg-black-100"
@@ -22,45 +23,22 @@
                 class="opacity-75"
                 fill="currentColor"
                 d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
-            ></path>
+            />
         </svg>
     </div>
 </template>
 
 <script setup>
-import { ref, onMounted, onUnmounted, computed } from "vue";
+import { computed, onMounted } from "vue";
 import store from "../store/index.js";
 import Sidebar from "./Sidebar.vue";
 import AdminHeader from "./AdminHeader.vue";
 
-const isSidebarOpen = ref(true);
-
 const currentUser = computed(() => store.state.user.data);
-
-function toggleSidebar() {
-    isSidebarOpen.value = !isSidebarOpen.value;
-}
 
 onMounted(() => {
     store.dispatch("getUser");
-
-    if (window.outerWidth < 768) {
-        isSidebarOpen.value = false;
-    }
-    window.addEventListener("resize", handleResize);
 });
-
-onUnmounted(() => {
-    window.removeEventListener("resize", handleResize);
-});
-
-function handleResize() {
-    if (window.outerWidth < 768) {
-        isSidebarOpen.value = false;
-    } else {
-        isSidebarOpen.value = true;
-    }
-}
 </script>
 
 <style scoped></style>
--- admin/src/components/Sidebar.vue
+++ admin/src/components/Sidebar.vue
@@ -1,9 +1,9 @@
 <template>
     <div
-        class="bg-indigo-700 text-gray-300 w-16 py-4 px-2 space-y-2 rounded-r-sm shadow-lg overflow-hidden transition-all duration-300"
+        class="bg-indigo-700 text-gray-300 w-16 py-4 px-2 space-y-2 rounded-r-sm shadow-lg"
     >
         <router-link
-             :to="{ name: 'app.orders' }"
+            :to="{ name: 'app.orders' }"
             class="flex items-center py-1 px-2 rounded hover:bg-black/30"
         >
             <HomeIcon class="h-5 w-5" />
@@ -39,13 +39,6 @@ import {
     UserIcon,
     ChartBarIcon,
 } from "@heroicons/vue/20/solid";
-
-defineProps({
-    isOpen: {
-        type: Boolean,
-        default: true,
-    },
-});
 </script>
 
 <style scoped></style>
