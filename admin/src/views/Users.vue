6455dba78282cb239d8d0019551513362b3b0a15
beybora <bora.b.sarikaya@gmail.com>
2025-06-10 22:06:21 +0200

    admin: add is_active field to users table

--- admin/src/views/Users.vue
+++ admin/src/views/Users.vue
@@ -9,6 +9,8 @@
                     <th class="w-1/4 px-4 py-2">Email</th>
                     <th class="w-1/4 px-4 py-2">Cart</th>
                     <th class="w-1/4 px-4 py-2">Orders</th>
+                    <th class="w-1/4 px-4 py-2">Status</th>
+                    <th class="w-1/4 px-4 py-2">Actions</th>
                 </tr>
             </thead>
             <tbody v-if="users.length > 0">
@@ -22,11 +24,25 @@
                         </span>
                     </td>
                     <td class="px-4 py-2">{{ user.orders_count }}</td>
+                    <td class="px-4 py-2">
+                        <span :class="user.is_active ? 'text-green-700' : 'text-red-600'">
+                            {{ user.is_active ? 'Active' : 'Inactive' }}
+                        </span>
+                    </td>
+                    <td class="px-4 py-2">
+                        <button
+                            @click="toggleActive(user)"
+                            :class="user.is_active ? 'bg-red-100 text-red-700 hover:bg-red-200' : 'bg-green-100 text-green-700 hover:bg-green-200'"
+                            class="px-3 py-1 rounded"
+                        >
+                            {{ user.is_active ? 'Deactivate' : 'Activate' }}
+                        </button>
+                    </td>
                 </tr>
             </tbody>
             <tbody v-else>
                 <tr>
-                    <td colspan="5" class="text-center text-gray-500 italic py-6">
+                    <td colspan="7" class="text-center text-gray-500 italic py-6">
                         No users found.
                     </td>
                 </tr>
@@ -44,4 +60,12 @@ onMounted(() => {
 });
 
 const users = computed(() => store.state.users.data);
+
+async function toggleActive(user) {
+    await store.dispatch("updateUser", {
+        id: user.id,
+        is_active: !user.is_active,
+    });
+    await store.dispatch("getShopUsers");
+}
 </script>
